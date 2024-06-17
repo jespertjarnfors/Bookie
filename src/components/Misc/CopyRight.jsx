b69baaa9928496b821f0412bb3772fc9d0ca4843
jespertjarnfors <139288555+jespertjarnfors@users.noreply.github.com>
2024-06-17 14:41:40 +1200

    Quick edit to the login page.

--- src/components/Misc/CopyRight.jsx
+++ src/components/Misc/CopyRight.jsx
@@ -1,9 +1,14 @@
 import Typography from "@mui/material/Typography";
 
 export function CopyRight() {
-  return (
+  return (<>
+  <Typography variant="body2" color="white" align="left" marginTop={"10px"}>
+  Username: test123 <br />
+  Password: 123test
+  </Typography>
     <Typography variant="body2" color="white" align="center" marginTop={"auto"} marginBottom={"20px"}>
       Copyright © Jesper Tjarnfors.
     </Typography>
+    </>
   );
 }
