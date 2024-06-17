be8f115b5f1a33e1b20409acf5855b5f97af645c
jespertjarnfors <139288555+jespertjarnfors@users.noreply.github.com>
2024-06-17 15:22:01 +1200

    Folder structure and vercel setup complete.

--- client/src/components/Misc/CopyRight.jsx
+++ client/src/components/Misc/CopyRight.jsx
@@ -2,7 +2,7 @@ import Typography from "@mui/material/Typography";
 
 export function CopyRight() {
   return (<>
-  <Typography variant="body2" color="white" align="left" marginTop={"10px"}>
+  <Typography variant="body2" color="white" marginTop={"10px"}>
   Username: test123 <br />
   Password: 123test
   </Typography>
